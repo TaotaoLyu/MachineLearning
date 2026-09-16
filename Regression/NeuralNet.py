37dea046d738aa84f8b83b0be20e357c87df45c8
Tao <148466661+TaotaoLyu@users.noreply.github.com>
2026-09-16 17:12:04 +0800

    Implement a regression model for COVID-19 case prediction.

---
+++ Regression/NeuralNet.py
@@ -0,0 +1,62 @@
+
+
+import torch.nn as nn
+import torch
+
+
+
+class NeuralNet(nn.Module):
+    ''' A simple fully-connected deep neural network '''
+    def __init__(self, input_dim):
+        super(NeuralNet, self).__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(128, 64),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Linear(64, 1)
+        )
+
+        # Mean squared error loss
+        self.criterion = nn.MSELoss(reduction='mean')
+
+    def forward(self, x):
+        ''' Given input of size (batch_size x input_dim), compute output of the network '''
+        return self.net(x).squeeze(1)
+
+    def cal_loss(self, pred, target):
+        ''' Calculate loss '''
+        # TODO: you may implement L1/L2 regularization here
+        return self.criterion(pred, target)
+    
+
+
+def dev(dv_set, model, device):
+    model.eval()                                # set model to evalutation mode
+    total_loss = 0
+    for x, y in dv_set:                         # iterate through the dataloader
+        x, y = x.to(device), y.to(device)       # move data to device (cpu/cuda)
+        with torch.no_grad():                   # disable gradient calculation
+            pred = model(x)                     # forward pass (compute output)
+            mse_loss = model.cal_loss(pred, y)  # compute loss
+        total_loss += mse_loss.detach().cpu().item() * len(x)  # accumulate loss
+    total_loss = total_loss / len(dv_set.dataset)              # compute averaged loss
+
+    return total_loss
+
+
+
+def test(tt_set, model, device):
+    model.eval()                                # set model to evalutation mode
+    preds = []
+    for x in tt_set:                            # iterate through the dataloader
+        x = x.to(device)                        # move data to device (cpu/cuda)
+        with torch.no_grad():                   # disable gradient calculation
+            pred = model(x)                     # forward pass (compute output)
+            preds.append(pred.detach().cpu())   # collect prediction
+    preds = torch.cat(preds, dim=0).numpy()     # concatenate all predictions and convert to a numpy array
+    return preds
---
+++ Regression/main.py
@@ -0,0 +1,123 @@
+# PyTorch
+import torch
+import torch.nn as nn
+# from torch.utils.data import Dataset, DataLoader
+
+# For data preprocess
+import numpy as np
+# import csv
+import os
+
+# For plotting
+# import matplotlib.pyplot as plt
+# from matplotlib.pyplot import figure
+
+from utils import *
+from ProcessData import *
+from NeuralNet import *
+
+
+tr_path = 'covid.train.csv'  # path to training data
+tt_path = 'covid.test.csv'   # path to testing data
+
+
+
+myseed = 42069  # set a random seed for reproducibility
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(myseed)
+torch.manual_seed(myseed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(myseed)
+
+
+    
+def train(tr_set, dv_set, model, config, device):
+    ''' DNN training '''
+
+    n_epochs = config['n_epochs']  # Maximum number of epochs
+
+    # Setup optimizer
+    optimizer = getattr(torch.optim, config['optimizer'])(
+        model.parameters(), **config['optim_hparas'])
+
+    min_mse = 1000.
+    loss_record = {'train': [], 'dev': []}      # for recording training loss
+    early_stop_cnt = 0
+    epoch = 0
+    while epoch < n_epochs:
+        model.train()                           # set model to training mode
+        for x, y in tr_set:                     # iterate through the dataloader
+            optimizer.zero_grad()               # set gradient to zero
+            x, y = x.to(device), y.to(device)   # move data to device (cpu/cuda)
+            pred = model(x)                     # forward pass (compute output)
+            mse_loss = model.cal_loss(pred, y)  # compute loss
+            mse_loss.backward()                 # compute gradient (backpropagation)
+            optimizer.step()                    # update model with optimizer
+            loss_record['train'].append(mse_loss.detach().cpu().item())
+
+        # After each epoch, test your model on the validation (development) set.
+        dev_mse = dev(dv_set, model, device)
+        if dev_mse < min_mse:
+            # Save model if your model improved
+            min_mse = dev_mse
+            print('Saving model (epoch = {:4d}, loss = {:.4f})'
+                .format(epoch + 1, min_mse))
+            torch.save(model.state_dict(), config['save_path'])  # Save model to specified path
+            early_stop_cnt = 0
+        else:
+            early_stop_cnt += 1
+
+        epoch += 1
+        loss_record['dev'].append(dev_mse)
+        if early_stop_cnt > config['early_stop']:
+            # Stop training if your model stops improving for "config['early_stop']" epochs.
+            break
+
+    print('Finished training after {} epochs'.format(epoch))
+    return min_mse, loss_record
+
+
+
+device = get_device()                 # get the current available device ('cpu' or 'cuda')
+os.makedirs('models', exist_ok=True)  # The trained model will be saved to ./models/
+target_only = True                   # TODO: Using 40 states & 2 tested_positive features
+
+# TODO: How to tune these hyper-parameters to improve your model's performance?
+config = {
+    'n_epochs': 2000,                # maximum number of epochs
+    'batch_size': 270,               # mini-batch size for dataloader
+    'optimizer': 'SGD',              # optimization algorithm (optimizer in torch.optim)
+    'optim_hparas': {                # hyper-parameters for the optimizer (depends on which optimizer you are using)
+        'lr': 0.001,                 # learning rate of SGD
+        'momentum': 0.9              # momentum for SGD
+    },
+    'early_stop': 20000,               # early stopping epochs (the number epochs since your model's last improvement)
+    'save_path': 'models/model.pth'  # your model will be saved here
+}
+
+
+
+tr_set = prep_dataloader(tr_path, 'train', config['batch_size'], target_only=target_only)
+dv_set = prep_dataloader(tr_path, 'dev', config['batch_size'], target_only=target_only)
+tt_set = prep_dataloader(tt_path, 'test', config['batch_size'], target_only=target_only)
+
+
+model = NeuralNet(tr_set.dataset.dim).to(device)  # Construct model and move to device
+# print(tr_set.dataset.dim)
+
+model_loss, model_loss_record = train(tr_set, dv_set, model, config, device)
+
+plot_learning_curve(model_loss_record, title='deep model')
+
+
+del model
+model = NeuralNet(tr_set.dataset.dim).to(device)
+ckpt = torch.load(config['save_path'], map_location='cpu')  # Load your best model
+model.load_state_dict(ckpt)
+plot_pred(dv_set, model, device)  # Show prediction on the validation set
+
+
+
+preds = test(tt_set, model, device)  # predict COVID-19 cases with your model
+save_pred(preds, 'pred.csv')         # save prediction file to pred.csv
